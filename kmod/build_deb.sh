591df97c4421a02d82fa3ef08ff2e051168394fa
Saulius Grusnys <saulius.grusnys@xtxmarkets.com>
2023-08-08 12:51:00 +0100

    add initial version of building kmod debian package

---
+++ kmod/build_deb.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+VERSION=${1:?}
+
+BASE_DIR="eggsfs-client-${VERSION}"
+MOD_DIR="${BASE_DIR}/lib/modules/$(uname -r)/fs/eggsfs"
+SVC_DIR="${BASE_DIR}/etc/systemd/system"
+MOD_LOAD_DIR="${BASE_DIR}/etc/modules-load.d"
+
+mkdir -p ${BASE_DIR}/DEBIAN
+cat debian/control | sed "s/##VERSION##/${VERSION}/g" > ${BASE_DIR}/DEBIAN/control
+cp debian/postinst ${BASE_DIR}/DEBIAN
+chmod +x ${BASE_DIR}/DEBIAN/postinst
+
+mkdir -p ${MOD_LOAD_DIR}
+cp debian/eggsfs_load.conf ${MOD_LOD_DIR}/eggsfs.conf
+
+mkdir -p ${MOD_DIR}
+cp eggsfs.ko ${MOD_DIR}
+
+mkdir -p ${SVC_DIR}
+cp debian/eggsfs-client.service ${SVC_DIR}
+
+mkdir -p ${BASE_DIR}/mnt/eggsfs
+
+dpkg-deb --build --root-owner-group ${BASE_DIR}

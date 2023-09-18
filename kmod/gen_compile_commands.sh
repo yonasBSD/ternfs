b43a31eb24adccc9fd21827b2ae9fb41ca97ff9c
Francesco Mazzoli <francesco.mazzoli@xtxmarkets.com>
2023-09-18 12:44:22 +0000

    Add script to generate `kmod/compile_commands.json`

---
+++ kmod/gen_compile_commands.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+set -x
+
+./linux/scripts/gen_compile_commands.py
+sed -i 's:-I .:-I ./linux:g' ./compile_commands.json
+sed -i 's:-include .:-include ./linux:g' ./compile_commands.json

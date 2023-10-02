203b95acb49c39b1c5c2568ae64de2feb5e08349
Francesco Mazzoli <francesco.mazzoli@xtxmarkets.com>
2023-10-02 20:03:00 +0000

    Disable block service Go conn reuse until #68 is fixed

--- go/lib/client.go
+++ go/lib/client.go
@@ -721,11 +721,13 @@ func (c *wrappedBlockConn) Close() error {
 }
 
 func (c *wrappedBlockConn) Put() {
-	if c.conn == nil {
-		return
-	}
-	c.conn.Put()
-	c.conn = nil
+	c.Close()
+	// Until we fix <internal-repo/issues/68>...
+	// if c.conn == nil {
+	// 	return
+	// }
+	// c.conn.Put()
+	// c.conn = nil
 }
 
 // These two will block until the connection gets freed -- you can't nest calls to these.

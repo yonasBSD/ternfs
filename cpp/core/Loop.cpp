66fe0a262118c8a2a0d52c43ab10762daa69f82c
Francesco Mazzoli <francesco.mazzoli@xtxmarkets.com>
2024-03-20 11:13:26 +0000

    Correct `pthread_timedjoin_np` handling

--- cpp/core/Loop.cpp
+++ cpp/core/Loop.cpp
@@ -139,15 +139,18 @@ void LoopThread::waitUntilStopped(std::vector<std::unique_ptr<LoopThread>>& loop
         for (;;) {
             int ret = pthread_timedjoin_np(loop->_thread, nullptr, &timeout);
             if (ret == EINTR) { continue; }
-            if (ret != 0 && ret == ETIMEDOUT) {
-                char name[16];
-                {
-                    int ret = pthread_getname_np(loop->_thread, name, sizeof(name));
-                    if (ret != 0) {
-                        throw EXPLICIT_SYSCALL_EXCEPTION(ret, "pthread_getname_np");
+            if (ret != 0) {
+                if (ret == ETIMEDOUT) {
+                    char name[16];
+                    {
+                        int ret = pthread_getname_np(loop->_thread, name, sizeof(name));
+                        if (ret != 0) {
+                            throw EXPLICIT_SYSCALL_EXCEPTION(ret, "pthread_getname_np");
+                        }
                     }
+                    throw EGGS_EXCEPTION("loop %s has not terminated in time, aborting", name);
                 }
-                throw EGGS_EXCEPTION("loop %s has not terminated in time, aborting", name);
+                throw EXPLICIT_SYSCALL_EXCEPTION(ret, "pthread_timedjoin_np");
             }
             break;
         }

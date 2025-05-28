4d6a966a71ca83f4c3a532f4aab011e12f1c24db
Saulius Grusnys <saulius.grusnys@xtxmarkets.com>
2025-05-28 05:44:16 +0000

    update disk replacement instructions for spinning disks

---
+++ go/lib/blocks.go
@@ -0,0 +1,17 @@
+package lib
+
+import (
+	"encoding/binary"
+	"xtx/eggsfs/msgs"
+)
+
+func BlockServiceIdFromKey(secretKey [16]byte) msgs.BlockServiceId {
+	// we don't really care about leaking part or all of the key -- the whole key business is
+	// to defend against bugs, not malicious agents.
+	//
+	// That said, we prefer to encode the knowledge of how to generate block service ids once,
+	// in the block service.
+	//
+	// Also, we remove the highest bit for the sake of SQLite, at least for now
+	return msgs.BlockServiceId(binary.LittleEndian.Uint64(secretKey[:8]) & uint64(0x7FFFFFFFFFFFFFFF))
+}

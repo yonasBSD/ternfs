752d9c074a6f264606bb3101a704016d2a2308b0
Joshua Leahy <joshua.leahy@xtxmarkets.com>
2025-09-09 15:42:02 +0000

    Sink code from XmonAgent.hpp to XmonAgent.cpp

--- cpp/core/XmonAgent.cpp
+++ cpp/core/XmonAgent.cpp
@@ -1,6 +1,7 @@
 #include "XmonAgent.hpp"
 #include "Assert.hpp"
-#include "Exception.hpp"
+
+#include <fcntl.h>
 
 struct XmonRequestHeader {
     int64_t alertId;
@@ -54,4 +55,67 @@ bool XmonRequest::read(int fd) {
     buf[header.messageLen] = '\0';
     message = std::string(buf);
     return true;
+}
+
+int64_t XmonAgent::_createAlert(XmonAppType appType, bool binnable, Duration quietPeriod, const std::string& message) {
+    int64_t aid = _alertId.fetch_add(1);
+    XmonRequest req;
+    req.appType = appType;
+    req.msgType = XmonRequestType::CREATE;
+    req.alertId = aid;
+    req.binnable = binnable;
+    req.message = message;
+    req.quietPeriod = quietPeriod;
+    req.write(_pipe[1]);
+    return aid;
+}
+
+XmonAgent::XmonAgent() : _alertId(1) {
+    if (pipe(_pipe) < 0) {
+        throw SYSCALL_EXCEPTION("pipe");
+    }
+    if (fcntl(readFd(), F_SETFL, O_NONBLOCK) < 0) {
+        throw SYSCALL_EXCEPTION("fcntl");
+    }
+}
+
+XmonAgent::~XmonAgent() {
+    if (close(_pipe[0]) < 0) {
+        std::cerr << "Could not close read end of pipe pipe (" << errno << ")" << std::endl;
+    }
+    if (close(_pipe[1]) < 0) {
+        std::cerr << "Could not close write end of pipe pipe (" << errno << ")" << std::endl;
+    }
+}
+
+void XmonAgent::raiseAlert(XmonAppType appType, const std::string& message) {
+    _createAlert(appType, true, 0, message);
+}
+
+void XmonAgent::updateAlert(XmonNCAlert& aid, const std::string& message) {
+    if (aid.alertId < 0) {
+        aid.alertId = _createAlert(aid.appType, false, aid.quietPeriod, message);
+    } else {
+        XmonRequest req;
+        req.appType = aid.appType;
+        req.msgType = XmonRequestType::UPDATE;
+        req.alertId = aid.alertId;
+        req.binnable = false;
+        req.message = message;
+        req.quietPeriod = 0;
+        req.write(_pipe[1]);
+    }
+}
+
+void XmonAgent::clearAlert(XmonNCAlert& aid) {
+    if (likely(aid.alertId < 0)) { return; }
+    XmonRequest req;
+    req.appType = aid.appType;
+    req.msgType = XmonRequestType::CLEAR;
+    req.alertId = aid.alertId;
+    req.binnable = false;
+    req.message = {};
+    req.quietPeriod = 0;
+    req.write(_pipe[1]);
+    aid.alertId = -1;
 }
--- cpp/core/XmonAgent.hpp
+++ cpp/core/XmonAgent.hpp
@@ -1,15 +1,10 @@
 #pragma once
 
-#include <fcntl.h>
+#include "Time.hpp"
+
 #include <stdint.h>
-#include <mutex>
-#include <deque>
 #include <atomic>
 #include <vector>
-
-#include "Common.hpp"
-#include "Exception.hpp"
-#include "Time.hpp"
 
 enum struct XmonRequestType : int32_t {
     CREATE = 0x4,
@@ -53,82 +48,20 @@ struct XmonNCAlert {
 
 struct XmonAgent {
 private:
-    int _pipe[2];
+    int _pipe[2]; // {readfd, writefd}
     std::atomic<int64_t> _alertId;
 
-    int _writeFd() const {
-        return _pipe[1];
-    }
-
-    void _sendRequest(const XmonRequest& req) {
-        req.write(_writeFd());
-    }
-
-    int64_t _createAlert(XmonAppType appType, bool binnable, Duration quietPeriod, const std::string& message) {
-        int64_t aid = _alertId.fetch_add(1);
-        XmonRequest req;
-        req.appType = appType;
-        req.msgType = XmonRequestType::CREATE;
-        req.alertId = aid;
-        req.binnable = binnable;
-        req.message = message;
-        req.quietPeriod = quietPeriod;
-        _sendRequest(req);
-        return aid;
-    }
+    int64_t _createAlert(XmonAppType appType, bool binnable, Duration quietPeriod, const std::string& message);
 
 public:
     static constexpr int64_t TOO_MANY_ALERTS_ALERT_ID = 0;
 
-    XmonAgent() : _alertId(1) {
-        if (pipe(_pipe) < 0) {
-            throw SYSCALL_EXCEPTION("pipe");
-        }
-        if (fcntl(readFd(), F_SETFL, O_NONBLOCK) < 0) {
-            throw SYSCALL_EXCEPTION("fcntl");
-        }
-    }
+    XmonAgent();
+    ~XmonAgent();
 
-    ~XmonAgent() {
-        if (close(_pipe[0]) < 0) {
-            std::cerr << "Could not close read end of pipe pipe (" << errno << ")" << std::endl;
-        }
-        if (close(_pipe[1]) < 0) {
-            std::cerr << "Could not close write end of pipe pipe (" << errno << ")" << std::endl;
-        }
-    }
-
-    void raiseAlert(XmonAppType appType, const std::string& message) {
-        _createAlert(appType, true, 0, message);
-    }
-
-    void updateAlert(XmonNCAlert& aid, const std::string& message) {
-        if (aid.alertId < 0) {
-            aid.alertId = _createAlert(aid.appType, false, aid.quietPeriod, message);
-        } else {
-            XmonRequest req;
-            req.appType = aid.appType;
-            req.msgType = XmonRequestType::UPDATE;
-            req.alertId = aid.alertId;
-            req.binnable = false;
-            req.message = message;
-            req.quietPeriod = 0;
-            _sendRequest(req);
-        }
-    }
-
-    void clearAlert(XmonNCAlert& aid) {
-        if (likely(aid.alertId < 0)) { return; }
-        XmonRequest req;
-        req.appType = aid.appType;
-        req.msgType = XmonRequestType::CLEAR;
-        req.alertId = aid.alertId;
-        req.binnable = false;
-        req.message = {};
-        req.quietPeriod = 0;
-        _sendRequest(req);
-        aid.alertId = -1;
-    }
+    void raiseAlert(XmonAppType appType, const std::string& message);
+    void updateAlert(XmonNCAlert& aid, const std::string& message);
+    void clearAlert(XmonNCAlert& aid);
 
     int readFd() const {
         return _pipe[0];

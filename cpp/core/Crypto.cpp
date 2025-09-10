af58e27615a7f78fd9a7276a94fa77b469359b93
Joshua Leahy <joshua.leahy@xtxmarkets.com>
2025-09-10 09:33:32 +0000

    Reference AES back to Intel's ICP library
    That libray was released under Apache 2.0, whereas the PDF unfortunately
    does not provide a license allowing for use of its example code.

--- cpp/core/Crypto.cpp
+++ cpp/core/Crypto.cpp
@@ -1,28 +1,22 @@
-// Code from
-// <https://www.intel.com/content/dam/doc/white-paper/advanced-encryption-standard-new-instructions-set-paper.pdf>.
+
 #include <emmintrin.h>
 #include <wmmintrin.h>
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/random.h>
 
 #include "Crypto.hpp"
 #include "Exception.hpp"
 
-
 void generateSecretKey(std::array<uint8_t, 16>& key) {
-    ssize_t read = getrandom(key.data(), key.size(), 0);
-    if (read < 0) {
-        throw SYSCALL_EXCEPTION("getrandom");
-    }
-    if (read != key.size()) {
-        // getrandom(2) states that once initialized you can always get up to 256 bytes.
-        throw TERN_EXCEPTION("could not read %s random bytes, read %s instead!", key.size(), read);
-    }
+   int ret = getentropy(key.data(), 16);
+   if (ret != 0) {
+       throw SYSCALL_EXCEPTION("getentropy");
+   }
 }
 
-inline __m128i AES_128_ASSIST(__m128i temp1, __m128i temp2) {
+// adapted from the Intel Cryptography Primitives Library
+static inline __m128i aes128_assist(__m128i temp1, __m128i temp2) {
     __m128i temp3;
     temp2 = _mm_shuffle_epi32(temp2 ,0xff);
     temp3 = _mm_slli_si128(temp1, 0x4);
@@ -35,41 +29,42 @@ inline __m128i AES_128_ASSIST(__m128i temp1, __m128i temp2) {
     return temp1;
 }
 
-void expandKey(const std::array<uint8_t, 16>& userkey, AES128Key& key) {
+// adapted from the Intel Cryptography Primitives Library
+void expandKey(const std::array<uint8_t, 16>& userKey, AES128Key& key) {
     __m128i temp1, temp2;
-    __m128i *Key_Schedule = (__m128i*)&key;
-    temp1 = _mm_loadu_si128((__m128i*)userkey.data());
-    Key_Schedule[0] = temp1;
+    __m128i *k = (__m128i*)key.key;
+    temp1 = _mm_loadu_si128((__m128i*)userKey.data());
+    k[0] = temp1;
     temp2 = _mm_aeskeygenassist_si128(temp1, 0x1);
-    temp1 = AES_128_ASSIST(temp1, temp2);
-    Key_Schedule[1] = temp1;
+    temp1 = aes128_assist(temp1, temp2);
+    k[1] = temp1;
     temp2 = _mm_aeskeygenassist_si128(temp1, 0x2);
-    temp1 = AES_128_ASSIST(temp1, temp2);
-    Key_Schedule[2] = temp1;
+    temp1 = aes128_assist(temp1, temp2);
+    k[2] = temp1;
     temp2 = _mm_aeskeygenassist_si128(temp1, 0x4);
-    temp1 = AES_128_ASSIST(temp1, temp2);
-    Key_Schedule[3] = temp1;
+    temp1 = aes128_assist(temp1, temp2);
+    k[3] = temp1;
     temp2 = _mm_aeskeygenassist_si128(temp1, 0x8);
-    temp1 = AES_128_ASSIST(temp1, temp2);
-    Key_Schedule[4] = temp1;
+    temp1 = aes128_assist(temp1, temp2);
+    k[4] = temp1;
     temp2 = _mm_aeskeygenassist_si128(temp1, 0x10);
-    temp1 = AES_128_ASSIST(temp1, temp2);
-    Key_Schedule[5] = temp1;
+    temp1 = aes128_assist(temp1, temp2);
+    k[5] = temp1;
     temp2 = _mm_aeskeygenassist_si128(temp1, 0x20);
-    temp1 = AES_128_ASSIST(temp1, temp2);
-    Key_Schedule[6] = temp1;
+    temp1 = aes128_assist(temp1, temp2);
+    k[6] = temp1;
     temp2 = _mm_aeskeygenassist_si128(temp1, 0x40);
-    temp1 = AES_128_ASSIST(temp1, temp2);
-    Key_Schedule[7] = temp1;
+    temp1 = aes128_assist(temp1, temp2);
+    k[7] = temp1;
     temp2 = _mm_aeskeygenassist_si128(temp1, 0x80);
-    temp1 = AES_128_ASSIST(temp1, temp2);
-    Key_Schedule[8] = temp1;
+    temp1 = aes128_assist(temp1, temp2);
+    k[8] = temp1;
     temp2 = _mm_aeskeygenassist_si128(temp1, 0x1b);
-    temp1 = AES_128_ASSIST(temp1, temp2);
-    Key_Schedule[9] = temp1;
+    temp1 = aes128_assist(temp1, temp2);
+    k[9] = temp1;
     temp2 = _mm_aeskeygenassist_si128(temp1, 0x36);
-    temp1 = AES_128_ASSIST(temp1, temp2);
-    Key_Schedule[10] = temp1;
+    temp1 = aes128_assist(temp1, temp2);
+    k[10] = temp1;
 }
 
 std::array<uint8_t, 8> cbcmac(const AES128Key& key, const uint8_t* data, size_t len) {
